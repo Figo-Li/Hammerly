19e0c73fcfbe2176937a7ced1593cabc92b19457
jqiwen <jqiwen00@gmail.com>
2026-03-06 16:07:25 -0500

    update user guide

--- hammerly-ui/src/pages/guide/page.tsx
+++ hammerly-ui/src/pages/guide/page.tsx
@@ -1,13 +1,15 @@
 import Header from '../../components/feature/Header';
 import Footer from '../../components/feature/Footer';
 import GuideHero from './components/GuideHero';
+import GuideSteps from './components/GuideSteps';
 
 export default function Guide() {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col">
+    <div className="min-h-screen bg-gray-50">
       <Header />
-      <main className='flex-grow'>
+      <main>
         <GuideHero />
+        <GuideSteps />
       </main>
       <Footer />
     </div>
